bf55ff99e995bc199c744727e79ae3b546833152
jas-rowlett90 <jas.rowlett90+portfolio@gmail.com>
2024-12-31 17:59:30 -0600

    block fix

---
+++ src/test/java/com/example/springk8scicd/SpringK8sCicdApplicationTests.java
@@ -0,0 +1,12 @@
+package com.example.springk8scicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringK8sCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
